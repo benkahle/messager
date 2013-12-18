f17862da89ffa8ff6412b7891270ef6f1f2d0474
benkahle <ben@kahle.org>
2013-12-17 22:35:30 -0800

    Started project. Got initial servers set up and generalized the server set up into a separate module, fairly neat.

---
+++ app.js
@@ -0,0 +1,58 @@
+var net = require('net');
+var path = require('path');
+var express = require('express');
+var chatServer = require('chat-server');
+
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
+var EventEmitter = require('events').EventEmitter;
+
+var emitter = new EventEmitter();
+var args = process.argv.slice(2);
+
+var server = chatServer.createServer({
+  httpServer: http,
+  tcpHost: args[0],
+  tcpPort: args[1]
+});
+
+server.start();
+
+// server.on('client connected', function(socket) {
+//   console.log("a client connected");
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
+http.listen(args[2]);
+
+// http.listen(80);
+// io.sockets.on('connection', function(socket) {
+//   socket.emit('message', 'hi there client');
+//   socket.on('message', function(data) {
+//     console.log('Data from socket.io client: '+ data);
+//   })
+// })
+
+// var server = net.Server();
+// server.listen(3000, 'localhost');
+
+// server.on('connection', function(socket) {
+//   socket.write("Hello Client");
+//   socket.on('data', function(data) {
+//     console.log("Data from client: " + data);
+//   });
+// });
+
+setTimeout(function(){
+  var connection = net.connect(3000, 'localhost', function() {
+    // connection.write("Hi Server");
+    // setInterval(function() {
+    //   connection.write('ping');
+    // }, 2000);
+    // connection.on('data', function(data) {
+    //   console.log("Data from server: " + data);
+    // })
+  });
+},2000)
+
